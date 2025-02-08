b40bd6c1f3a6f58672b476cda190139062380c37
KgQe <grucelkonrad8@gmail.com>
2025-02-08 16:21:37 +0100

    App: small structure fixes
    -include batteryu_level.h only if CONFIG_BATTERY_LEVEL_DRV=y
    -delete uceecesseray comments

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,5 +1,8 @@
 #include <zephyr/sys/printk.h>
+
+#ifdef CONFIG_BATTERY_LEVEL_DRV
 #include "battery_level.h"
+#endif
 
 int main(){
     printk("dummy app");
--- drivers/battery_level/src/battery_level.c
+++ drivers/battery_level/src/battery_level.c
@@ -1,4 +1,5 @@
 #include <zephyr/kernel.h>
+#include "battery_level.h"
 
 const struct device *adc_dev = DEVICE_DT_GET(DT_NODELABEL(adc));
 

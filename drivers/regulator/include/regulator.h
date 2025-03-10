9750f7437f8ed70419ddb3a7910685b733e2d898
KgQe <grucelkonrad8@gmail.com>
2025-03-10 22:04:56 +0100

    Drivers: regulator and imu
    -collect indetification data in regulator.c
    -fix imu error with wornd integration of gyro - still gyro diffr to fix
    -regulation and identification  based on angle-rad and angle_dt - rad/s

--- drivers/regulator/include/regulator.h
+++ drivers/regulator/include/regulator.h
@@ -7,6 +7,23 @@
 extern "C" {
 #endif
 
+#ifdef CONFIG_MODEL_IDENTIFICATION_DRV
+#include "imu.h"
+
+struct identification_regulator_data
+{
+    float dt;
+    float pwm;
+    float angle;
+    float angle_dt;
+};
+
+typedef void (*regulator_data_updated_cb_t)(struct identification_regulator_data data);
+
+void
+new_pwm_cb_register(regulator_data_updated_cb_t _new_pwm_cb);
+#endif
+
 struct pid_regulator_parameters
 {
     float Kp;
@@ -22,8 +39,14 @@ void
 new_nus_parameters_received_for_regulator(const uint8_t* data, uint16_t len);
 #endif  // CONFIG_LOG_OVER_BLE
 
+#ifdef CONFIG_MODEL_IDENTIFICATION_DRV
+void
+new_imu_angle_for_regulator(struct identification_data data);
+
+#else
 void
 new_imu_angle_for_regulator(float _angle);
+#endif
 
 void
 new_pid_regulator_parameters_cb_register(regulator_params_updated_cb_t _new_pid_regulator_parameters_cb);

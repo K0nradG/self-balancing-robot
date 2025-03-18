1c89da401e9b6c245eb694cfc82463e8c190cb95
KgQe <grucelkonrad8@gmail.com>
2025-03-18 21:53:03 +0100

    Regulator: PID
    -find otimal gains values

--- drivers/regulator/include/regulator_utils.h
+++ drivers/regulator/include/regulator_utils.h
@@ -8,11 +8,12 @@ extern "C" {
 
 #define MS_TO_SECONDS 0.001f
 #define M_PI 3.14159265358979323846f
-#define N_coeff (2.0f * M_PI * (float)CONFIG_FILTER_CUTOFF_FREQUENCY)         // Filter coefficient in [rad/s]
+#define N_coeff 59840                                                         // Filter coefficient in [rad/s]
 #define N_dt (N_coeff * (float)CONFIG_REGULATOR_SAMPLE_TIME * MS_TO_SECONDS)  // [rad]
 #define ALPHA      \
     N_dt / (1.0f + \
             N_dt)  // Alpha coefficients to be used directly by the low-pass filter: alpha = (N * dt) / (1 + N * dt)
+
 #define DEG_TO_RAD (M_PI / 180.0f)
 #define ANGLE_OFFSET 95.0f
 

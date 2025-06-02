3ed8e4f4d10d4691d48d26f78553377b36ae1684
KgQe <grucelkonrad8@gmail.com>
2025-06-02 17:54:31 +0200

    drivers:regulator: gains and setpoint adjustments

--- drivers/regulator/src/pid.c
+++ drivers/regulator/src/pid.c
@@ -33,7 +33,7 @@ static pid_regulator g_balance_regulator = {
     .last_time  = 0,
     .integral   = 0.0f,
     .last_error = 0.0f,
-    .parameters = {.Kp = 1500.0, .Ki = 800.0f, .Kd = 0.1f, .setpoint = -6.8f}};  //-7.4485
+    .parameters = {.Kp = 1300.0, .Ki = 800.0f, .Kd = 0.1f, .setpoint = -7.0f}};  //-6.8 , -7.4485
 
 // TODO: Setpoint not larger than += 45 degrees from previous point works.
 static pid_regulator g_rotation_regulator = {

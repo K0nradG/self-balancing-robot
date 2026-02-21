bcc43db2aa7a94f5827ab9c3485919cbcfe980dd
KgQe <grucelkonrad8@gmail.com>
2026-02-21 22:53:15 +0100

    mathematical_model:remove old scripts

--- Sources/scripts/matlab/LQR_tuning_continous.m
+++
@@ -1,9 +0,0 @@
-init;
-
-K = lqr(ss_continous.A, ss_continous.B, Q, R);
-
-disp('Kalman Gain:');
-disp(K)
-
-eig_vals = eig(ss_continous.A - ss_continous.B * K);
-isstable(ss(ss_continous.A - ss_continous.B * K, ss_continous.B, ss_continous.C, ss_continous.D))

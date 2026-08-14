7e0296ed8ddd0ba05873692e9243e0767d84e3ef
Filip <filip.dymczyk@o2.pl>
2026-08-14 18:07:20 +0200

    removal of emptry includes from __init__.py

--- dfu_utils/__init__.py
+++ dfu_utils/__init__.py
@@ -1,11 +1,1 @@
 #!/usr/bin/env python3
-
-from .config import *
-from .progress_bar import ProgressBar
-from .smpmgr_wrapper import run_smpmgr, run_smpmgr_with_retry
-from .firmware_utils import (
-    extract_firmware_from_zip,
-    extract_slot_hash,
-    read_secondary_image_hash,
-)
-from .dfu_operations import DFUOperations
